c6dce0b1f206d1d1d956586bfad39791ad0ae45a
malqqin <malqqin@163.com>
2024-08-29 08:57:40 +0800

    feat: ✨home

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <CommonPageLayout title="我的" :leftArrow="false">
     <!-- 个人信息部分 -->
-    <view>这是一个自定义导航栏页面</view>
+    <view class="mt-4">这是一个自定义导航栏页面</view>
     自定义图标：
     <view class="i-ri-add-fill" />
     <up-button type="primary" @click="toOrderDetail">跳转分包页面</up-button>
